48a953b67ce77b6efa4a60d071a4f87541f0edc7
Konstantins <k.tarasjuks@gmail.com>
2022-09-22 13:12:28 +0300

    added testfile.txt

---
+++ slice_ex1.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    our_string = 'My name is something must go here'
+    result = our_string[0:10]
+    print(result + ' Konstantin')
+    name = 'Konstantin'
+    print('{1} {0}'.format(result, name))
+
+
+
